64b74ccbaf07d57b97cc7949db2b6b90b486ad35
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-08-30 11:25:41 +0100

    api func and search ready

--- lesson-1/src/App.js
+++ lesson-1/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {SearchBar} from './components'
+import youtube from './api/youtube'
 
 const App = () => {
+  const [videos,setVideos] = useState([])
+  const onSearchTerm = async  (term) => {
+    // console.log(term);
+    const response = await youtube.get('/search' , {
+      params:{
+        q:term
+      }
+    } )
+    // console.log(response.data)
+    setVideos(response.data.items)
+  }
   return (
     <div className='ui container' >
-      <SearchBar />
+      <SearchBar onFormSubmit={onSearchTerm} />
     </div>
   )
 }
