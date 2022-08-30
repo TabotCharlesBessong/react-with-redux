25d6963dff59ce25279804984d801da922229f14
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-08-30 14:21:16 +0100

    default search and default detail video

--- lesson-1/src/App.js
+++ lesson-1/src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {SearchBar, VideoDetail, VideoList} from './components'
 import youtube from './api/youtube'
+import search from './constant/constant'
 
 const App = () => {
   const [videos,setVideos] = useState([])
   const [selectedVideo,setSelectedVideo] = useState(null)
+  const selectedNumber = Math.floor(Math.random() * videos.length )
+  const selectedNumber1 = Math.floor(Math.random() * search.length);
   const onSearchTerm = async  (term) => {
     // console.log(term);
     const response = await youtube.get('/search' , {
@@ -14,12 +17,17 @@ const App = () => {
     } )
     console.log(response.data.items)
     setVideos(response.data.items)
+    setSelectedVideo(response.data.items[selectedNumber])
   }
 
   const onVideoSelect = (video)=>{
     // console.log('video app',video)
     setSelectedVideo(video)
   }
+
+  useEffect(()=>{
+    onSearchTerm(search[selectedNumber1])
+  },[])
   return (
 		<div className="ui container">
 			<SearchBar onFormSubmit={onSearchTerm} />
---
+++ lesson-1/src/constant/constant.js
@@ -0,0 +1,20 @@
+
+const search = [
+ 'us open',
+ 'rafael nadal',
+ 'dragon ball super',
+ 'jujutsu kaisen',
+ 'roland garros',
+ 'gordon ramsey',
+ 'inazuma',
+ 'wimbledon',
+ 'rogger federer',
+ 'saint seiya',
+ 'messi',
+ 'react',
+ 'free code camp',
+ 'frontend',
+ 'banana',
+]
+
+export default search
