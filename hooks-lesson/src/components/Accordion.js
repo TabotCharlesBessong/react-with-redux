79c6fe6b7cd3fc85e385baaa19bc9367f4c6ced2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-09-01 12:22:16 +0100

    excercise plus toggle functionalities

--- hooks-lesson/src/components/Accordion.js
+++ hooks-lesson/src/components/Accordion.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null)
+  const [count, setCount] = useState(0)
 
   const handleClick = (index) => {
-    console.log('i was clicked',index)}
+    console.log('i was clicked',index)
+    setActiveIndex(index)
+  }
+
+  const handleCount = (count) => {
+    count = count + 1
+    setCount(count)
+  }
   
   const renderedItems = items.map((item,index) => {
+
+    const active  = index === activeIndex ? 'active' : ''
     return (
       <React.Fragment key={item.title}>
-        <div onClick={() => handleClick(index)} className="title active">
+        <div onClick={() => handleClick(index)} className={"title " + active }>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={"content" + active }>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+     <div className="ui styled accordion">{renderedItems}
+     <div className="button">
+      <button onClick={()=> handleCount(count) } >click me</button>
+      <h2>Current count</h2>: <span> {count} </span>
+     </div>
+    </div>
+  )
+  
 };
 
 export default Accordion;
