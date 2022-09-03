98ef84ef33c0abb1eff9839ced53db452d1c28eb
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-09-03 10:05:55 +0100

    finishing touches on search

--- hooks-lesson/src/components/Search.js
+++ hooks-lesson/src/components/Search.js
@@ -29,10 +29,37 @@ const Search = () => {
       })
       setResults(data.query.search)
     }
-    if(term) {
-      searchWiki();
+
+    if(term && !results.length){
+      searchWiki()
+    }else{
+      const timeout = setTimeout(() => {
+				if (term) {
+					searchWiki()
+				}
+			}, 1000)
+
+			return () => {
+				clearTimeout(timeout)
+			}
     }
-  },[term])
+    
+    
+  },[results.length, term])
+
+  const renderedResults = results.map((res)=>(
+    <div key={res.pageid} className="item">
+      <div className="right floated content">
+        <a href={`https://en.wikipedia.org?curid=${res.pageid}`} className="ui button">Go</a>
+      </div>
+      <div className="content">
+        <div className="header">
+          {res.title}
+        </div>
+        <span dangerouslySetInnerHTML={{__html:res.snippet}} ></span>
+      </div>
+    </div>
+  ))
   return (
     <div>
       <div className="ui form">
@@ -40,6 +67,9 @@ const Search = () => {
           <label htmlFor="">Enter Search Term</label>
           <input value={term} onChange={e => setTerm(e.target.value)}   type="text" className="input" />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
