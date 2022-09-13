05a8faa23372e31c852e8533ce606c9deb999f3e
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-13 11:31:49 +0200

    created the stream file s

---
+++ advancedRouteLesson/client/src/components/StreamShowDetail.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const StreamShowDetail = () => {
+  return (
+    <div>StreamShowDetail</div>
+  )
+}
+
+export default StreamShowDetail
--- advancedRouteLesson/client/src/components/index.js
+++ advancedRouteLesson/client/src/components/index.js
@@ -1,2 +1,8 @@
 
 export { default as Header } from './Header'
+export { default as StreamCreate } from './StreamCreate'
+export { default as StreamDelete } from './StreamDelete'
+export { default as StreamEdit } from './StreamEdit'
+export { default as StreamShow } from './StreamShow'
+export { default as StreamShowDetail } from "./StreamShowDetail";
+export { default as StreamList } from "./StreamList";
