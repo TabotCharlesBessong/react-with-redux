d04ad099dbce7745f9f2950ccaee7711f87a7542
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-09-13 11:55:28 +0200

    routes..

--- advancedRouteLesson/client/src/App.js
+++ advancedRouteLesson/client/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch } from 'react-router-dom'
-import {Header} from './components'
+import {Header, StreamCreate, StreamDelete, StreamList, StreamShow, StreamShowDetail} from './components'
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <h1>Hello world</h1>
-        <Switch>
-          <Route path='/' component={Header} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+		<div>
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route path="/" exact component={StreamList} />
+					<Route path="/stream/show" component={StreamShow} />
+					<Route path="/stream/show/detail" component={StreamShowDetail} />
+					<Route path="/stream/edit" component={StreamShow} />
+					<Route path="/stream/delete" component={StreamDelete} />
+					<Route path="/stream/create" component={StreamCreate} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App
--- advancedRouteLesson/client/src/components/StreamCreate.js
+++ advancedRouteLesson/client/src/components/StreamCreate.js
@@ -4,7 +4,7 @@ import React from 'react'
 const StreamCreate = () => {
   return (
     <div>
-      
+      <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, adipisci? </h1>
     </div>
   )
 }
